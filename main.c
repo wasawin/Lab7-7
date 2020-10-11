ceba2effac3f0f9560a6ddfbc246d0d6f4f3ae1a
wasawin <wasawin.14@gmail.com>
2020-10-11 15:06:16 +0000

    add Desc sorting

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 int main(void) 
 {
  int i, j;
- int temp[7][2];
- //int temp[7][2]={40,30,39,28,38,27,37,26,36,25,35,24,42,23};
+ //int temp[7][2];
+ int temp[7][2]={40,30,39,28,38,27,37,26,36,25,35,24,42,23};
  int Max, Min;
  int midH[7], midL[7];
  float SumH, SumL;
@@ -33,13 +33,13 @@ printf("******************************** \n");
          printf("High Temp %s%d%d ",day[i],i,j);
          scanf("%d", &temp[i][j]);
          midH[i]= temp[i][j];
-         
+         SumH += temp[i][j];
           }
          else{
          printf("Low  Temp %s%d%d ",day[i],i,j);
          scanf("%d", &temp[i][j]);
          midL[i] = temp[i][j];
-         
+         SumL += temp[i][j];
          }
       }
     }
@@ -49,15 +49,18 @@ printf("******************************** \n");
   for (i=0; i<7; i++)
     for (j=0; j<2; j++){
         if(temp[i][j]> Max)
-         Max= temp[i][j];
-         if(temp[i][j]<Min)
-         Min= temp[i][j]; 
+         Max=temp[i][j];
+         if(temp[i][j]< Min)
+         Min=temp[i][j]; 
           }
 
+  for (i=0; i<7; i++){
+    printf("midh%d [%d]\n",midH[i],i);
+  }
 
 
-  SumH=temp[0][0]+temp[1][0]+temp[2][0]+temp[3][0]+temp[4][0]+temp[5][0]+temp[6][0];
-  SumL=(temp[0][1]+temp[1][1]+temp[2][1]+temp[3][1]+temp[4][1]+temp[5][1]+temp[6][1]);
+  //SumH=temp[0][0]+temp[1][0]+temp[2][0]+temp[3][0]+temp[4][0]+temp[5][0]+temp[6][0];
+  //SumL=(temp[0][1]+temp[1][1]+temp[2][1]+temp[3][1]+temp[4][1]+temp[5][1]+temp[6][1]);
   AvgH=SumH/7;
   AvgL=SumL/7;
   
@@ -67,18 +70,36 @@ printf("******************************** \n");
   printf("Average High Temperature is %f C\n",AvgH);
   printf("Average Low  Temperature is %f C\n",AvgL);
    
-   int swap;
+   int swaph,swapl;
+   //////////////High
 for(i=0; i<7; i++){
-		for(j=i+1; j<7; j++){
+		for(j=i+1; j<6; j++){
 			if(midH[i]>midH[j]) //Swap
-				swap = midH[i];
-				midH[i] = midH[j];
-				midH[j] = swap;
+				swaph=midH[i];
+				midH[i]=midH[j];
+				midH[j]=swaph;
 			
 		}
 	}
-  
+   for (i=0; i<7; i++){
+    printf("midh%d [%d]\n",midH[i],i);
+  }
+  //////////////low
+  for(i=0;i<7;i++){
+   for(j=i+1;j<7;j++){
+     if(midL[i]>midL[j]){
+       swapl=midL[i];
+       midL[i]=midL[j];
+       midL[j]=swapl;
+       }
+      }
+    }
+  for (i=0; i<7; i++){
+    printf("midl%d [%d]\n",midL[i],i);
+  }
 
+
+  
   //High temp[0][0] temp[1][0] temp[2][0] temp[3][0] temp[4][0] temp[5][0] temp[6][0] 
   //low  temp[0][1] temp[1][1] temp[2][1] temp[3][1] temp[4][1] temp[5][1] temp[6][1]
 
